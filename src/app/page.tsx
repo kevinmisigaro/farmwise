dc7a1d90fee21412eef18aa6ee1ffff643748808
Kevin Misigaro <kevin@databalk.nu>
2024-04-05 13:39:56 +0200

    added sections and images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,8 +29,44 @@ export default function Home() {
       </div>
       {/* end of hero section */}
 
+      {/* benefits section  */}
+      <div>
+        <div className="flex flex-col md:flex-row items-stretch">
+          <div className="basis-1 md:basis-1/2 px-5 md:px-20 py-32">
+            <h2 className={`${mali.className} text-5xl`}>
+              Why Wait? Your Farming Dream is Just a Click Away
+            </h2>
+            <p className="mt-10 text-justify">
+              Transform your vision of owning a prosperous farm into reality
+              with Farmwise. Our streamlined process, backed by comprehensive
+              support, makes farming a seamless, rewarding experience. With
+              limited spaces available, now is the moment to act. Visit us at
+              &nbsp;
+              <a href="www.farmwise.tz" target="_blank" className="underline">
+                www.farmwise.tz
+              </a>
+              , subscribe, and embark on your farming journey today. Embrace the
+              future of farming with Farmwise — a promise of success!
+            </p>
+          </div>
+          <div className="basis-1 md:basis-1/2 mb-10">
+            <div className="bg-[url('/assets/images/farm3.jpg')] h-[60vh] bg-cover"></div>
+
+            <div className="card bg-[#173d25] border-8 border-[#fdd51f] max-w-sm -mt-5 animate-bounce">
+              <div className="card-body text-white text-center">
+                <div className="stat-value">400+</div>
+                <div className={`stat-desc text-lg ${mali.className}`}>
+                  Pure Organic Products
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* end of benefits section */}
+
       {/* why choose us section */}
-      <div className="py-28 bg-[#fefdf3]">
+      {/* <div className="py-28 bg-[#fefdf3]">
         <h1 className={`text-center text-5xl ${mali.className}`}>
           Why Are We Better <br /> Than Others?
         </h1>
@@ -84,65 +120,118 @@ export default function Home() {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
       {/* end of why choose us section */}
 
       {/* services section  */}
       <div className="py-32 bg-[#173d25]">
         <h1 className={`text-center text-5xl text-white ${mali.className}`}>
-        How Farmwise works:
-          <br/> Effortless Farming at your Fingertips
+          How Farmwise works:
+          <br /> Effortless Farming at your Fingertips
         </h1>
+
+        <div className="px-5 md:px-20 py-10 text-white">
+          <p>
+            At Farmwise, we've streamlined the path to successful farming for
+            busy professionals like you. Our innovative online Farm services
+            platform is designed to make agriculture accessible, no matter your
+            schedule or location. Here's how you can dive into farming,
+            hassle-free:
+          </p>
+        </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-x-20 gap-y-10 mt-10 px-20">
           <div className="card w-full bg-[#00200d] shadow-xl">
             <div className="card-body p-12">
-              <h3 className={`${mali.className} text-white text-lg`}>
-                Agricultural Products
-              </h3>
+              <div className="flex flex-row items-center gap-x-5">
+                <img src="/assets/icons/add-user.png" className="w-8" />
+                <h3 className={`${mali.className} text-white text-lg`}>
+                  Subscribe and Select
+                </h3>
+              </div>
 
               <p className="text-[#81af93] mt-3">
-                Welcome to Dosner Organic Farms.We are a direct distributor of
-                100% Organic herbs produce.
+                Start your journey by subscribing to our online services. Browse
+                through our extensive list of mechanized and irrigated farms
+                across the country and select the one that speaks to your
+                agricultural aspirations.
               </p>
             </div>
           </div>
 
           <div className="card w-full bg-[#00200d] shadow-xl">
             <div className="card-body p-12">
-              <h3 className={`${mali.className} text-white text-lg`}>
-                Fresh Vegetables
-              </h3>
+              <div className="flex flex-row items-center gap-x-5">
+                <img src="/assets/icons/custom.png" className="w-8" />
+                <h3 className={`${mali.className} text-white text-lg`}>
+                  Customize Your Farm
+                </h3>
+              </div>
 
               <p className="text-[#81af93] mt-3">
-                Welcome to Dosner Organic Farms.We are a direct distributor of
-                100% Organic herbs produce.
+                Decide on the acreage that fits your goals. Whether it's a
+                modest plot or an expansive field, Farmwise is equipped to bring
+                your farming vision to life.
               </p>
             </div>
           </div>
 
           <div className="card w-full bg-[#00200d] shadow-xl">
             <div className="card-body p-12">
-              <h3 className={`${mali.className} text-white text-lg`}>
-                ADairy Products
-              </h3>
+              <div className="flex flex-row items-center gap-x-5">
+                <img src="/assets/icons/relax.png" className="w-8" />
+                <h3 className={`${mali.className} text-white text-lg`}>
+                  Sit Back, We've Got This
+                </h3>
+              </div>
 
               <p className="text-[#81af93] mt-3">
-                Welcome to Dosner Organic Farms.We are a direct distributor of
-                100% Organic herbs produce.
+                Once you've made your choice, Farmwise takes over. We provide
+                all necessary quality inputs, including premium seeds,
+                environmentally friendly fertilizers, and pesticides. Plus, our
+                agronomic extension services are there to ensure your farm
+                thrives.
               </p>
             </div>
           </div>
 
           <div className="card w-full bg-[#00200d] shadow-xl">
             <div className="card-body p-12">
-              <h3 className={`${mali.className} text-white text-lg`}>
-                Organic Corn
-              </h3>
+              <div className="flex flex-row items-center gap-x-5">
+                <img src="/assets/icons/customer-support.png" className="w-8" />
+                <h3 className={`${mali.className} text-white text-lg`}>
+                  Expert Care On-Demand
+                </h3>
+              </div>
 
               <p className="text-[#81af93] mt-3">
-                Welcome to Dosner Organic Farms.We are a direct distributor of
-                100% Organic herbs produce.
+                Understanding your busy lifestyle and the physical distance from
+                your farm, we offer the option to hire certified agricultural
+                experts directly through our platform. They'll tend to your
+                farm, applying expert knowledge and care, ensuring optimal
+                growth and yield.
+              </p>
+            </div>
+          </div>
+
+          <div className="card w-full bg-[#00200d] shadow-xl">
+            <div className="card-body p-12">
+              <div className="flex flex-row items-center gap-x-5">
+                <img
+                  src="/assets/icons/combine-harvester.png"
+                  className="w-8"
+                />
+                <h3 className={`${mali.className} text-white text-lg`}>
+                  Harvest Your Success
+                </h3>
+              </div>
+
+              <p className="text-[#81af93] mt-3">
+                The fruits of your investment are entirely yours. At harvest,
+                you're free to enjoy the produce, share it, or even sell it back
+                to us. Should you choose to sell, we ensure a smooth
+                transaction, crediting your proceeds directly to your designated
+                bank account.
               </p>
             </div>
           </div>
@@ -163,7 +252,7 @@ export default function Home() {
               <div>
                 <div className="flex flex-row items-center mb-5 gap-x-5">
                   <img
-                    src="/assets/icons/Icon 3.png"
+                    src="/assets/icons/Farm.png"
                     className="text-white max-w-20"
                   />
                   <div className="font-bold text-lg">
@@ -182,7 +271,7 @@ export default function Home() {
               <div>
                 <div className="flex flex-row items-center mb-5 gap-x-5">
                   <img
-                    src="/assets/icons/Icon 2.png"
+                    src="/assets/icons/Com Sup.png"
                     className="text-white w-10"
                   />
                   <div className="font-bold">Comprehensive Support</div>
@@ -199,7 +288,7 @@ export default function Home() {
               <div>
                 <div className="flex flex-row items-center mb-5 gap-x-5">
                   <img
-                    src="/assets/icons/Icon4.png"
+                    src="assets/icons/Recy.png"
                     className="text-white w-10"
                   />
                   <div className="font-bold">
@@ -217,7 +306,7 @@ export default function Home() {
               <div>
                 <div className="flex flex-row items-center mb-5 gap-x-5">
                   <img
-                    src="/assets/icons/Icon 1.png"
+                    src="/assets/icons/Success.png"
                     className="text-white w-10"
                   />
                   <div className="font-bold">Guaranteed Success</div>
@@ -234,42 +323,6 @@ export default function Home() {
           </div>
         </div>
       </div>
-
-      {/* benefits section  */}
-      <div>
-        <div className="flex flex-col md:flex-row items-stretch">
-          <div className="basis-1 md:basis-1/2 px-5 md:px-20 py-32">
-            <h2 className={`${mali.className} text-5xl`}>
-              Why Wait? Your Farming Dream is Just a Click Away
-            </h2>
-            <p className="mt-10 text-justify">
-              Transform your vision of owning a prosperous farm into reality
-              with Farmwise. Our streamlined process, backed by comprehensive
-              support, makes farming a seamless, rewarding experience. With
-              limited spaces available, now is the moment to act. Visit us at
-              &nbsp;
-              <a href="www.farmwise.tz" target="_blank" className="underline">
-                www.farmwise.tz
-              </a>
-              , subscribe, and embark on your farming journey today. Embrace the
-              future of farming with Farmwise — a promise of success!
-            </p>
-          </div>
-          <div className="basis-1 md:basis-1/2 mb-10">
-            <div className="bg-[url('/assets/images/farm3.jpg')] h-[60vh] bg-cover"></div>
-
-            <div className="card bg-[#173d25] border-8 border-[#fdd51f] max-w-sm -mt-5 animate-bounce">
-              <div className="card-body text-white text-center">
-                <div className="stat-value">400+</div>
-                <div className={`stat-desc text-lg ${mali.className}`}>
-                  Pure Organic Products
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* end of benefits section */}
 
       {/* start of testimonial */}
       <div className="px-5 md:px-20 py-20 bg-[#fcfaf0]">
@@ -318,23 +371,24 @@ export default function Home() {
         <div className="flex items-center gap-x-5 mb-5">
           <img
             className="mask mask-squircle w-[3.5rem] h-[3.5rem]"
-            src="https://daisyui.com/images/stock/photo-1567653418876-5bb0e566e1c2.jpg"
+            src="/assets/icons/icons8-phone-50.png"
           />
           <div>
             <h3>Helpline and Support</h3>
-            <h2 className="font-bold text-xl">+88 013 00 44 51</h2>
+            <h2 className="font-bold text-lg">+88 013 00 44 51</h2>
           </div>
         </div>
 
         <div className="flex items-center gap-x-5 mb-5">
           <img
             className="mask mask-squircle w-[3.5rem] h-[3.5rem]"
-            src="https://daisyui.com/images/stock/photo-1567653418876-5bb0e566e1c2.jpg"
+            src="/assets/icons/icons8-location-50.png"
           />
           <div>
             <h3>Our Address</h3>
-            <h2 className="font-bold text-xl">
-            Msasani Tower, Postal code 14111, <br/> Plot number 503/1, P.O. Box 38556, <br/> Dar Es Salaam, Tanzania
+            <h2 className="font-bold text-lg">
+              Msasani Tower, Postal code 14111, <br /> Plot number 503/1, P.O.
+              Box 38556, <br /> Dar Es Salaam, Tanzania
             </h2>
           </div>
         </div>
@@ -342,11 +396,11 @@ export default function Home() {
         <div className="flex items-center gap-x-5 mb-5">
           <img
             className="mask mask-squircle w-[3.5rem] h-[3.5rem]"
-            src="https://daisyui.com/images/stock/photo-1567653418876-5bb0e566e1c2.jpg"
+            src="/assets/icons/icons8-telegram-app-50.png"
           />
           <div>
             <h3>Send an Email</h3>
-            <h2 className="font-bold text-xl">Support@gmail.com</h2>
+            <h2 className="font-bold text-lg">Support@gmail.com</h2>
           </div>
         </div>
       </div>
