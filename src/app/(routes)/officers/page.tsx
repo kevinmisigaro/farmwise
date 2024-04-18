677b7bf4a1ad38a9b3fb7243faeb3ecd237a59b1
Kevin Misigaro <kmisigaro@outlook.com>
2024-04-19 01:37:12 +0200

    refactoring

--- src/app/(routes)/officers/page.tsx
+++ src/app/(routes)/officers/page.tsx
@@ -11,13 +11,21 @@ function Page() {
         <div className="hero-content text-center text-neutral-content text-white">
           <div className="max-w-7xl">
             <h1 className="mb-5 text-xl md:text-4xl font-bold text-center">
-            If you're an agricultural graduate from an accredited college and interested in becoming a Farm Officer, please leave your details below.
+              If you're an agricultural graduate from an accredited college and
+              interested in becoming a Farm Officer, please leave
+              your details below.
             </h1>
           </div>
         </div>
       </div>
       <div className="px-5 md:px-20 py-10 flex flex-row justify-center">
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSekLGiA52Pxo0LlriUZloh6H8BD3IuRTbSIKUcqrk3X6BLk2Q/viewform?embedded=true" width="1000" height="2000" >Loading…</iframe>
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLSekLGiA52Pxo0LlriUZloh6H8BD3IuRTbSIKUcqrk3X6BLk2Q/viewform?embedded=true"
+          width="1000"
+          height="2000"
+        >
+          Loading…
+        </iframe>
       </div>
     </div>
   );
